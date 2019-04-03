8295179f535042a17d48c06768e8b738df36773f
DKJTR <j1i90423@hotmail.com>
2019-04-03 16:01:43 +0800

    top10 tags

--- maplotlib_boxplot.py
+++ maplotlib_boxplot.py
@@ -26,14 +26,14 @@ fig = plt.figure()
 graph = plt.boxplot([tag1,tag2, tag3, tag4, tag5, tag6, tag7, tag8 ,tag9, tag10], labels = ['新聞雜誌:商業與經濟新聞',
                                                                                             '地區:台灣北部','新聞雜誌:地方新聞',
                                                                                             '地區:日本','展覽:商務貿易',
-                                                                                            '地區:歐美','生涯階段:工作階段(青壯年)'
-                                                                                            '新聞雜誌:國際新聞','地區:台灣南部'
+                                                                                            '地區:歐美','生涯階段:工作階段(青壯年)',
+                                                                                            '新聞雜誌:國際新聞','地區:台灣南部',
                                                                                             '旅遊:國內旅遊'], sym='')
 plt.boxplot([tag1,tag2, tag3, tag4, tag5, tag6, tag7, tag8 ,tag9, tag10], labels = ['新聞雜誌:商業與經濟新聞',
                                                                                             '地區:台灣北部','新聞雜誌:地方新聞',
                                                                                             '地區:日本','展覽:商務貿易',
-                                                                                            '地區:歐美','生涯階段:工作階段(青壯年)'
-                                                                                            '新聞雜誌:國際新聞','地區:台灣南部'
+                                                                                            '地區:歐美','生涯階段:工作階段(青壯年)',
+                                                                                            '新聞雜誌:國際新聞','地區:台灣南部',
                                                                                             '旅遊:國內旅遊'], sym='')
 plt.xticks(rotation = 45)
 # plt.grid(axis = 'y')
@@ -60,5 +60,7 @@ for line in graph['whiskers']:
          va='bottom', 
          ha='left')     
 
+
+plt.title("Top 10 Tags Box Plot (by uid count")
 plt.show()
 plt.savefig("C:\\Users\\DKJTR\\Desktop\\tag_score_standardize\\result.png")
