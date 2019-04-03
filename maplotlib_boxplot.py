d5f8f76e2a2f9e798b86d3f5bdc3f7cb412cbac5
DKJTR <j1i90423@hotmail.com>
2019-04-03 15:54:23 +0800

    top10 tags

--- maplotlib_boxplot.py
+++ maplotlib_boxplot.py
@@ -8,17 +8,35 @@ plt.rcParams['font.sans-serif'] = ['Microsoft JhengHei']
 plt.rcParams['axes.unicode_minus'] = False
 
 big_table = pd.read_csv("C:\\Users\\DKJTR\\Desktop\\tag_score_standardize\\finished_big_table.csv")
+big_table.fillna(0, inplace = True)
+tag1 = big_table['新聞雜誌:商業與經濟新聞']
+tag2 = big_table['地區:台灣北部']
+tag3 = big_table['新聞雜誌:地方新聞']
+tag4 = big_table['地區:日本']
+tag5 = big_table['展覽:商務貿易']
+tag6 = big_table['地區:歐美']
+tag7 = big_table['生涯階段:工作階段(青壯年)']
+tag8 = big_table['新聞雜誌:國際新聞']
+tag9 = big_table['地區:台灣南部']
+tag10 = big_table['旅遊:國內旅遊']
 
-tag1 = big_table['新聞雜誌:商業與經濟新聞'].fillna(0)
-tag2 = big_table['地區:台灣北部'].fillna(0)
 
-# 把兩個資料序列畫成 boxplot
+# 把資料序列畫成 boxplot
 fig = plt.figure()
-graph = plt.boxplot([tag1,tag2], labels = ['新聞雜誌:商業與經濟新聞','地區:台灣北部'], sym='')
-plt.boxplot([tag1,tag2], labels = ['新聞雜誌:商業與經濟新聞','地區:台灣北部'], sym='')
+graph = plt.boxplot([tag1,tag2, tag3, tag4, tag5, tag6, tag7, tag8 ,tag9, tag10], labels = ['新聞雜誌:商業與經濟新聞',
+                                                                                            '地區:台灣北部','新聞雜誌:地方新聞',
+                                                                                            '地區:日本','展覽:商務貿易',
+                                                                                            '地區:歐美','生涯階段:工作階段(青壯年)'
+                                                                                            '新聞雜誌:國際新聞','地區:台灣南部'
+                                                                                            '旅遊:國內旅遊'], sym='')
+plt.boxplot([tag1,tag2, tag3, tag4, tag5, tag6, tag7, tag8 ,tag9, tag10], labels = ['新聞雜誌:商業與經濟新聞',
+                                                                                            '地區:台灣北部','新聞雜誌:地方新聞',
+                                                                                            '地區:日本','展覽:商務貿易',
+                                                                                            '地區:歐美','生涯階段:工作階段(青壯年)'
+                                                                                            '新聞雜誌:國際新聞','地區:台灣南部'
+                                                                                            '旅遊:國內旅遊'], sym='')
 plt.xticks(rotation = 45)
 # plt.grid(axis = 'y')
-
 
 for line in graph['medians']:
     # get position data for median line
@@ -43,6 +61,4 @@ for line in graph['whiskers']:
          ha='left')     
 
 plt.show()
-
-
-print("Hello")
+plt.savefig("C:\\Users\\DKJTR\\Desktop\\tag_score_standardize\\result.png")
